b9f7ccee3e8205acf3bf1e549a0e4729a580c6de
Tatiana <tatigerzens@gmail.com>
2024-10-18 10:24:48 -0300

    updated feature

--- src/components/products/ProductList.vue
+++ src/components/products/ProductList.vue
@@ -1,11 +1,38 @@
-
-
 <template>
-    <div>
+    <v-card class="mx-auto" max-width="300">
 
-    </div>
+        <v-list lines="two">
+            <v-list-item v-for="product in products" 
+                            :key="product.id" 
+                            :title="product.title"
+                            ></v-list-item>
+        </v-list>
+    </v-card>
 </template>
+
 
 <script setup lang="ts">
 
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+
+interface Product {
+    id: number,
+    title: string
+}
+
+const products = ref<Product[]>([]);
+
+
+onMounted(() => {
+    axios.get("https://dummyjson.com/products/")
+        .then(response => products.value = response.data)
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+})
+
 </script>
+
+

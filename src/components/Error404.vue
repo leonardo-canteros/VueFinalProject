0c1adfb7bea82f33df75fefaa2caa3f0cb9f95ec
Tatiana <tatigerzens@gmail.com>
2024-10-20 20:13:15 -0300

    feat: add ErrorComponent with basic styling and layout

--- src/components/Error404.vue
+++ src/components/Error404.vue
@@ -1,12 +1,48 @@
 <script setup lang="ts">
+
+import ButtonComponent from '@/components/ButtonComponent.vue';
+
 </script>
 
 <template>
-    <div>
-
-        
+    <div class="container-error">
+        <div class="error-status">            
+            <h1>404</h1>
+            <h2>Page not found!</h2>
+        </div>
+        <div class="error-message">
+            <p>We're sorry, the page you requested could not be found.
+                Please go back to the homepage!
+            </p>
+            <ButtonComponent></ButtonComponent>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.container-error {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center; 
+}
+
+h1 {
+    font-size: 4rem;
+}
+
+h2 {
+    font-size: 2rem;
+}
+
+.error-message {
+    margin-top: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    width: 400px; 
+    max-width: 600px; 
+}
+
 </style>

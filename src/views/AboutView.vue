a3ffe218d9df55e4d867d964c415618abc93847f
Tatiana <tatigerzens@gmail.com>
2024-10-21 18:57:28 -0300

    feat: update About Us view component with new content and styles

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>About Us</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod nulla beatae, porro alias velit laborum
-      vel ipsum, expedita eaque quisquam deleniti pariatur accusamus eligendi, dolorum quas labore. Atque, quaerat?</p>
+    <p>At [Your Store Name], we believe that music is a universal language, and everyone should have access to the
+      instruments that bring their creativity to life. Our mission is to provide musicians of all levels, from beginners
+      to professionals, with high-quality instruments and accessories. Whether you're looking to start your musical
+      journey or perfect your craft, we're here to support you every step of the way.</p>
   </div>
 </template>
 
@@ -11,6 +13,23 @@
 
 </script>
 
-<style></style>
+<style scoped>
+div {
+  padding: 40px 0px;
+}
+
+h1 {
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 1.2rem;
+  line-height: 1.6;
+  text-align: justify;
+  color: #333;
+}
+
+</style>
 
 

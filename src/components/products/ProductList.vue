eda2a37eca0c73dc38c4d55bc7f4fee9e959b2c9
Tatiana <tatigerzens@gmail.com>
2024-10-22 16:35:10 -0300

    Created ProductList to render multiple ProductCard in a grid

--- src/components/products/ProductList.vue
+++ src/components/products/ProductList.vue
@@ -1,61 +1,15 @@
 <template>
-    <v-table>
-    <thead>
-      <tr>
-        <th class="text-left">
-          Id
-        </th>
-        <th class="text-left">
-          Title
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="item in products"
-        :key="item.title"
-      >
-        <td>{{ item.id }}</td>
-        <td>{{ item.title }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+  <div>
+      <p>hola</p>
+  </div>
 </template>
+
 
 
 <script setup lang="ts">
 
-import { onMounted, ref } from 'vue';
-import axios from 'axios';
-
-
-interface Product {
-    id: number,
-    title: string
-}
-
-const products = ref<Product[]>([]);
-
-
-const getData = async()=> {
-
-    try{
-       const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
-       if(response.status==200){
-          products.value = response.data
-       }else{
-          console.log("Not found.")
-       }
-    } catch(error)
-    {
-      console.log(error)
-    }
-}
-
-onMounted(() => {
-    getData()
-})
-
 </script>
 
+<style scoped>
 
+</style>

0283588cd3d919c57f05a4321d80ebe3b82ea1bb
Tatiana <tatigerzens@gmail.com>
2024-10-23 10:59:52 -0300

    Added Carousel component for displaying featured items

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container fluid>
+        <v-carousel>
+            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+
+            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+
+            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+        </v-carousel>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+</script>

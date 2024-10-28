302206d8b0adc3064c297aa95efe7f793920a51f
Tatiana <tatigerzens@gmail.com>
2024-10-28 11:21:58 -0300

    Updated footercomponent with new content/styling

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-footer class="bg-blue-darken-4">
+      <v-row justify="center" no-gutters>
+        <v-btn
+
+          class="mx-2"
+          color="white"
+          rounded="xl"
+          variant="text"
+        >
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+            © {{ new Date().getFullYear() }} App Store Music. All rights reserved.
+        </v-col>
+      </v-row>
+    </v-footer>
+  </template>
+
+
+<script setup lang="ts">
+/*     export default {
+      data: () => ({
+        links: [
+          'Home',
+          'About Us',
+          'Team',
+          'Services',
+          'Blog',
+          'Contact Us',
+        ],
+      }),
+    } */
+</script>

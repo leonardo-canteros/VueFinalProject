67044d3c0902b3111396dfa5ff699b010db7dbd4
Tatiana <tatigerzens@gmail.com>
2024-10-28 11:10:59 -0300

    Updated CarouselComponent with new images

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,14 +1,19 @@
 <template>
     <v-container fluid>
         <v-carousel>
-            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+            <v-carousel-item :src="imgUrl" cover></v-carousel-item>
 
-            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+            <v-carousel-item :src="imgUrl2" cover></v-carousel-item>
 
-            <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+            <v-carousel-item :src="imgUrl3" cover></v-carousel-item>
         </v-carousel>
     </v-container>
 </template>
 
 <script setup lang="ts">
+
+import imgUrl from '@/assets/images/ImageStore01.jpg';
+import imgUrl2 from '@/assets/images/ImageStore02.jpg';
+import imgUrl3 from '@/assets/images/ImageStore03.jpg';
+
 </script>

69ea3990c8c8249c97d17497cb51ae80542f4c29
Tatiana <tatigerzens@gmail.com>
2024-11-06 15:03:12 -0300

    Updated SearchProducts component to include responsive design using v-col

--- src/components/products/SearchProducts.vue
+++ src/components/products/SearchProducts.vue
@@ -1,24 +1,26 @@
 <template>
   <v-sheet class="mx-auto" width="900">
     <v-form @submit.prevent="emitSearch">
+      <v-row>
+        
+        <v-col cols="12" md="8" lg="9">
+          <v-text-field 
+            type="text" 
+            placeholder="Search"
+            v-model="searchQuery"
+          ></v-text-field>
+        </v-col>
 
-      <v-text-field 
-        type="text" 
-        placeholder="Search"
-        v-model="searchQuery"
-
-        ></v-text-field>
-
-      <v-btn class="mt-2" 
-            type="submit"
-            
-            block
-        >Search</v-btn
-      >
+        <v-col cols="12" md="4" lg="3">
+          <v-btn class="mt-2" 
+                 type="submit"
+                 block
+          >Search</v-btn>
+        </v-col>
+      </v-row>
     </v-form>
   </v-sheet>
 </template>
-
 
 
 <script setup lang="ts">

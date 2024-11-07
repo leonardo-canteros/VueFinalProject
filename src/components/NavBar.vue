a0480c89ca10d1a70bcccb0e693c75882c3d3054
Tatiana <tatigerzens@gmail.com>
2024-11-07 11:32:41 -0300

    updated NavBar component with routerLink

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,41 +1,67 @@
-<script setup lang="ts">
-
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
-
 <template>
-  <v-toolbar image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" dark prominent>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <div id="app">
+        <v-app>
 
-    <v-toolbar-title>App Store Music</v-toolbar-title>
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Music Menu</v-toolbar-title>
+            <v-spacer></v-spacer>
 
-    <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+              <RouterLink v-for="item in menu" :key="item.icon" :to="item.link" class="text-white">
+                <v-btn flat>{{ item.title }}</v-btn>
+              </RouterLink>
+            </v-toolbar-items>
 
-    <v-btn>
-      <RouterLink to="/" class="text-white">Home</RouterLink>
-    </v-btn>
-    <v-btn>
-      <RouterLink to="/about" class="text-white">About</RouterLink>
-    </v-btn>
-    <v-btn>
-      <RouterLink to="/products" class="text-white">Products</RouterLink>
-    </v-btn>
-    <v-btn>
-      <RouterLink to="/userData" class="text-white">User Data</RouterLink>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-export</v-icon>
-    </v-btn>
-  </v-toolbar>
 
-  <RouterView />
+            <v-menu class="hidden-md-and-up">
+              <template v-slot:activator="{ on }">
+                <v-toolbar-side-icon v-on="on"></v-toolbar-side-icon>
+              </template>
+              <v-list>
+                <v-list-item v-for="item in menu" :key="item.icon" :to="item.link">
+                  <RouterLink :to="item.link" class="text-decoration-none">
+                    <v-list-item-content>
+                      <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                  </RouterLink>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+
+          </v-toolbar>
+
+          <v-content>
+            <v-container fluid fill-height>
+              <v-layout>
+
+                <RouterView />
+              </v-layout>
+
+            </v-container>
+          </v-content>
+        </v-app>
+      </div>
 </template>
 
 
+
+<script setup lang="ts">
+
+import { RouterLink, RouterView } from 'vue-router';
+import { ref } from 'vue';
+
+
+const menu = ref([
+  { title: 'Home', link: '/', icon: 'mdi-home' },
+  { title: 'About', link: '/about', icon: 'mdi-information' },
+  { title: 'Products', link: '/products', icon: 'mdi-cart' },
+])
+
+</script>
+
+
+
 <style scoped>
-nav a {
-  text-decoration: none;
-  margin-right: 15px;
-}
+
+
 </style>

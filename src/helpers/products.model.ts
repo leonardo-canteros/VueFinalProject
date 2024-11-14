3fce5ac0b8d746f096263fe1ad6adaa0b2161405
Tatiana <tatigerzens@gmail.com>
2024-11-14 11:19:49 -0300

    updated model for products

--- src/helpers/products.model.ts
+++ src/helpers/products.model.ts
@@ -1,21 +1,17 @@
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 
-const apiUrl = "https://upper-serena-fastapi-ecommerce-6026090d.koyeb.app/api/";
-
-export const getProductsAll = async function () {
-  return await axios.get(`${apiUrl}products/`);
-};
 
 const authStore = useAuthStore();
 
-// POST request using fetch with set headers
+
 const http = axios.create({
   baseURL: "https://upper-serena-fastapi-ecommerce-6026090d.koyeb.app/api/",
   headers: {
     Authorization: "Bearer " + authStore.getToken(),
   },
 });
+
 
 class TutorialDataService {
   getAll() {

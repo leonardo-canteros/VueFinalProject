ced10bd49f9ce8dd9b72cbc28b6c2720aed48184
Tatiana <tatigerzens@gmail.com>
2024-11-17 14:14:12 -0300

    fix(products): resolve issue with filter function in product section

--- src/stores/ProductsStore.ts
+++ src/stores/ProductsStore.ts
@@ -3,97 +3,118 @@ import { ref } from "vue";
 import TutorialDataService from "@/helpers/products.model";
 
 export const useProductsListStore = defineStore("productsStore", () => {
-  
-  const listProducts = ref([]);
-  const loading = ref(false);
-  const error = ref<string | null>(null);
+        const listProducts = ref([]);
+        const loading = ref(false);
+        const error = ref<string | null>(null);
 
-  async function fetchAllProducts() {
-    loading.value = true;
-    try {
-      const result = await TutorialDataService.getAll();
-      console.log("resultado", result.data.response);
+        async function fetchAllProducts() {
+          loading.value = true;
+          try {
+            const result = await TutorialDataService.getAll();
 
-      listProducts.value = result.data.response;
-    } catch (error) {
-      error.value = "Error fetching products";
-    } finally {
-      loading.value = false;
-    }
-  }
+            listProducts.value = result.data.response;
+          } catch (error) {
+            error.value = "Error fetching products";
+          } finally {
+            loading.value = false;
+          }
+        }
 
-  async function addProduct(product) {
-    try {
-      const respo = await TutorialDataService.create(product);
+        async function addProduct(product) {
+          try {
+            const respo = await TutorialDataService.create(product);
 
-      console.log("Respuesta: ---->", respo.data.response);
+            const newProduct = respo.data.response || respo.data;
 
-      const newProduct = respo.data.response || respo.data;
+            console.log("Nuevo producto:", newProduct);
 
-      listProducts.value.push(newProduct);
-    } catch (err) {
-      console.error("Error adding product:", err);
-      error.value = "Error adding product.";
-    }
-  }
+            listProducts.value.push(newProduct);
 
-  async function updateProduct(productId: any, product: any) {
-    try {
-      const updateProdResponse = await TutorialDataService.update(productId, product);
+            console.log("Lista actualizada:", listProducts.value);
+          } catch (err) {
+            console.error("Error adding product:", err);
+            error.value = "Error adding product.";
+          }
+        }
 
-      console.log("Respuesta: ---->", updateProdResponse.data.response);
+        async function updateProduct(productId: any, product: any) {
+          try {
+            const updateProdResponse = await TutorialDataService.update(
+              productId,
+              product
+            );
 
-      const productEdited = updateProdResponse.data.response || updateProdResponse.data;
+            console.log("Respuesta: ---->", updateProdResponse.data.response);
 
-      const indexOfProductToReplace = listProducts.value.findIndex((productItem) => {
-        return productItem.id === productId;
-      });
+            const productEdited =
+              updateProdResponse.data.response || updateProdResponse.data;
 
-      listProducts.value[indexOfProductToReplace] = productEdited;
+            const indexOfProductToReplace = listProducts.value.findIndex(
+              (productItem) => {
+                return productItem.id === productId;
+              }
+            );
 
+            listProducts.value[indexOfProductToReplace] = productEdited;
+          } catch (err) {
+            console.error("Error adding product:", err);
+            error.value = "Error adding product.";
+          }
+        }
 
-    } catch (err) {
-      console.error("Error adding product:", err);
-      error.value = "Error adding product.";
-    }
-  }
+        async function getProductId(id: any) {
+          try {
+            if (!listProducts.value || listProducts.value.length === 0) {
+              await TutorialDataService.get(id);
+            }
 
-  async function getProductId(id: any) {
-    try {
-      if (!listProducts.value || listProducts.value.length === 0) {
-        await TutorialDataService.get(id);
-      }
+            return listProducts.value.find((product) => product.id === id);
+          } catch (err) {
+            error.value = "Failed to fetch products.";
+          }
+        }
 
-      return listProducts.value.find((product) => product.id === id);
-    } catch (err) {
-      error.value = "Failed to fetch products.";
-    }
-  }
+        const originalProducts = ref([]);
 
-  function filterListProduct(searchQuery: string) {
-    if (searchQuery.trim() === "") {
-      listProducts.value;
-      return;
-    }
-    listProducts.value = listProducts.value.filter((product) => {
-      return product.name.toLowerCase().includes(searchQuery.toLowerCase());
-    });
-  }
+        function filterListProduct(searchQuery: any) {
+          if (originalProducts.value.length === 0) {
+            originalProducts.value = [...listProducts.value];
+          }
 
-  function clearProducts() {
-    listProducts.value = [];
-  }
+          if (searchQuery.trim() === "") {
+            listProducts.value = [...originalProducts.value];
+            return;
+          }
 
-  
-  return {
-    fetchAllProducts,
-    getProductId,
-    addProduct,
-    updateProduct,
-    filterListProduct,
-    clearProducts,
-    listProducts,
-    error,
-    loading,
-  };
+          listProducts.value = originalProducts.value.filter((product) =>
+            product.name.toLowerCase().includes(searchQuery.toLowerCase())
+          );
+        }
+
+        /*  function filterListProduct(searchQuery: string) {
+          if (searchQuery.trim() === "") {
+            listProducts.value;
+            return;
+          }
+          listProducts.value = listProducts.value.filter((product) => {
+            return product.name.toLowerCase().includes(searchQuery.toLowerCase());
+          });
+        } */
+
+        function clearProducts() {
+          listProducts.value = [];
+        }
+
+        return {
+          fetchAllProducts,
+          getProductId,
+          addProduct,
+          updateProduct,
+
+          filterListProduct,
+          clearProducts,
+          listProducts,
+          error,
+          loading,
+        };
 });

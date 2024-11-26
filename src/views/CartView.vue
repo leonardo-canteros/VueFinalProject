9a5bf642ae86d2cbbc20d742a3b262a2635b6816
leonardo <leonardocanteros2001@gmail.com>
2024-11-26 17:19:18 -0300

    se actualiza cart view y componentes

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,18 +1,15 @@
-<template>
+<template >
   <v-container class="d-flex flex-column justify-center">
     <h1 class="my-2">Mi Carrito</h1>
-
+    
     <!-- Solo muestra los productos si cartProducts tiene productos -->
     <v-list v-if="cartProducts.order_products.length > 0" dense>
-      <CartItem
-        v-for="product in cartProducts.order_products"
-        :key="product.id"
-        :product="product"
-      />
+      <CartItem v-for="product in cartProducts.order_products"
+       :key="product.product_id" :product="product" />
     </v-list>
-    
+
     <!-- Si no hay productos en el carrito, muestra un mensaje -->
-    <v-alert v-else type="info">Tu carrito está vacío</v-alert>
+    <v-alert v-else="cartProducts.order_products.length === 0" type="info">Tu carrito está vacío</v-alert>
 
     <!-- Aquí pasamos solo el arreglo de productos -->
     <CartSuma :product="cartProducts.order_products" />
@@ -23,19 +20,37 @@
 import CartSuma from "@/components/products/CartSuma.vue";
 import CartItem from "@/components/products/cartItem.vue";
 import { useCartStore } from "@/stores/cart";
-import { onMounted } from "vue";
+import type { Orders01 } from "@/stores/cart";
+import { ref, onMounted } from "vue";
 import { useAuthStore } from "@/stores/auth";
 
 const authStore = useAuthStore();
-const store = useCartStore();
-const cartProducts = store.cartProducts;    
-const userId = authStore.getUserId();
+const store = useCartStore();  
 
-console.log("ID del usuario:", userId);
+const cartProducts = ref<Orders01>({
+    customer_id: "",
+    order_products: [],
+    status: "",
+    id: "",
+  });
 
-onMounted(() => {
-  store.fetchCartProducts(userId);
-});
+console.log("cartProducts:", cartProducts.value);
+console.log("cartProducts.order_products.length", cartProducts.value.order_products.length);
 
 
+// onMounted para inicializar el carrito
+try {
+    onMounted(async () => {
+        const userId = authStore.getUserId();
+        console.log("userId", userId);
+        const response = await store.fetchCartProducts(userId);
+        console.log("Respuesta del carrito:", response);
+        cartProducts.value = response;
+        console.log("cartProducts.value", cartProducts.value);
+        return response;
+    })
+  } catch (error) {
+      console.error("Error al cargar los productos del carrito:", error);
+    }
+  
 </script>
